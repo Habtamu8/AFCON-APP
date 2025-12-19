1beb811f881aeebf86a3beff2717ee4bbf689256
Habtamu8 <habtamuwoldeamanuel8@gmail.com>
2025-12-19 09:14:28 +0300

    creating DB with Subabase

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,19 @@
 import 'package:afcon_app/features/Login/login_screen.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
-  runApp(const MyApp());
+  await Supabase.initialize(
+    url: 'https://uoaaxktetguzreuxcfxc.supabase.co',
+    anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVvYWF4a3RldGd1enJldXhjZnhjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjYxMTI4NTAsImV4cCI6MjA4MTY4ODg1MH0.POxRDF9AF1pMjfp91m3uHkopsEBMWGr0_z2gg6cP3w4',
+  );
+  runApp(const ProviderScope(child: MyApp()));
 }
+
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
