e527314eae6e6c84a7f3f7a23ed53426f9fff10e
Habtamu8 <habtamuwoldeamanuel8@gmail.com>
2025-12-19 08:45:11 +0300

    Intialize Firestore

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,13 @@
 import 'package:afcon_app/features/Login/login_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
-
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
